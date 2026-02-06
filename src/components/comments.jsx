c173f898a4939ce9b3178d3f10ae08fe07f0c98d
Abdul Fatawu Salifu <sfatawu625@gmail.com>
2026-02-06 20:00:33 +0000

    last

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -3,50 +3,57 @@ import TimeAgo from "react-timeago";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import { BASE_URL } from "../apis/apis";
 
 function CommentsBoxComponent(comments) {
-
-   
-    const JusNowCalculator = (timestamp) => {
-        const currentTime = new Date();
-        const timeCreated = new Date(timestamp);
-        const reminder_milicons = currentTime - timeCreated;
-        if (reminder_milicons <= 60000) {
-            return "Just Now";
-        }
-        else {
-            return <TimeAgo date={timestamp}/>
-        }
+  const JusNowCalculator = (timestamp) => {
+    const currentTime = new Date();
+    const timeCreated = new Date(timestamp);
+    const reminder_milicons = currentTime - timeCreated;
+    if (reminder_milicons <= 60000) {
+      return "Just Now";
+    } else {
+      return <TimeAgo date={timestamp} />;
     }
+  };
 
-    return (
-        <div className={styles.CommentCard} key={comments.comments.comment.id}>
-            <div className={styles.SignleComment}>
-                <div className={styles.CommeterProfile}>
-                    <div className={styles.CommenterProfilePicture}></div>
-                    <div className={styles.CommenterName}>
-                        <h3>
-                            <Link to={`/user/${comments.comments.commenter.id}`}>
-                            {comments.comments.commenter.firstname}{" "}
-                            {comments.comments.commenter.lastname}
-                            </Link>
-                        </h3>
-                    </div>
+  return (
+    <div className={styles.CommentCard} key={comments.comments.comment.id}>
+      <div className={styles.SignleComment}>
+        <div className={styles.CommeterProfile}>
+          <div className={styles.CommenterProfilePicture}>
+            {comments.comments.commenter.profile.status ? (
+              <img
+                src={`${BASE_URL}/src/uploads/${comments.comments.commenter.profile.media?.filename}`}
+                alt={`${comments.comments.commenter.firstname} profile picture`}
+              />
+            ) : (
+              <img
+                src={`${BASE_URL}/src/uploads/no_profile.jpg`}
+                alt={`${comments.comments.commenter.firstname} profile picture`}
+              />
+            )}
+          </div>
+          <div className={styles.CommenterName}>
+            <h3>
+              <Link to={`/user/${comments.comments.commenter.id}`}>
+                {comments.comments.commenter.firstname}{" "}
+                {comments.comments.commenter.lastname}
+              </Link>
+            </h3>
+          </div>
 
-                    <div className={styles.CommentPostedTime}>
-                         <p>
-                            {JusNowCalculator(comments.comments?.comment?.time_created)}
-                         </p>
-                    </div>
-                </div>
-                <div className={styles.CommenterComment}>
-                    <p>{comments.comments.comment.comment}</p>
-                </div>
-                <hr />
-            </div>
-
+          <div className={styles.CommentPostedTime}>
+            <p>{JusNowCalculator(comments.comments?.comment?.time_created)}</p>
+          </div>
         </div>
-    );
+        <div className={styles.CommenterComment}>
+          <p>{comments.comments.comment.comment}</p>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
 }
 
 export default CommentsBoxComponent;
