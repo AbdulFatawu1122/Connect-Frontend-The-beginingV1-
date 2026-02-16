b788f2fd4b66b97de9cd1cc42d3fd76231293429
Abdul Fatawu Salifu <sfatawu625@gmail.com>
2026-02-16 09:04:27 +0000

    adding analystics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
 
   //When to deploy this should be uncommented StrictMode
-  //<StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
- // </StrictMode>
+ </StrictMode>
 )
